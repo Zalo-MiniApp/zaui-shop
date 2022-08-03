a0ae61893616a51a5a707f3fb4b0bcb9c537b1d8
quanky99 <quanky99@gmail.com>
2022-08-03 18:03:34 +0700

    format eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default () => {
-  return defineConfig({
+export default () => defineConfig({
     root: './src',
     base: '',
     plugins: [react()],
   })
-}
