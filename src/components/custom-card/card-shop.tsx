9d3ce0992eaa5e003a170bf92f994e34660d4f14
quanldk <quanldk@vng.com.vn>
2022-12-26 14:44:10 +0700

    add configuration for open chat

--- src/components/custom-card/card-shop.tsx
+++ src/components/custom-card/card-shop.tsx
@@ -2,34 +2,48 @@ import React from 'react';
 import { Button, Icon } from 'zmp-ui';
 import { Store } from '../../models';
 import { getImgUrl } from '../../utils';
+import api from 'zmp-sdk';
+import { getConfig } from '../config-provider';
+import { DEFAULT_OA_ID } from '../../constants';
 
-const CardShop = ({ storeInfo }: { storeInfo: Store }) => (
-  <div className="flex flex-row justify-between items-center p-4 bg-white">
-    {storeInfo && (
-      <div className="flex flex-row items-center">
-        <img
-          src={getImgUrl(storeInfo.logoStore)}
-          alt="store-img"
-          className=" rounded-full object-cover w-[60px] h-[60px]"
-        />
-        <div className=" pl-4">
-          <div className=" text-base font-medium pb-1">{storeInfo.nameStore}</div>
-          <div className=" text-sm font-normal text-gray-500 pb-1">
-            {storeInfo.followers} theo dõi
-          </div>
-          <div className=" flex flex-row text-sm font-normal  text-gray-500">
-            <div className="flex items-center justify-center">
-              <Icon icon="zi-location-solid" size={12} />
+const CardShop = ({ storeInfo }: { storeInfo: Store }) => {
+  const handleOpenChat = () => {
+    const oaId: string = getConfig((c) => c.template.oaIDtoOpenChat || '');
+
+    api.openChat({
+      type: 'oa',
+      id: oaId || DEFAULT_OA_ID,
+    });
+  };
+
+  return (
+    <div className="flex flex-row justify-between items-center p-4 bg-white">
+      {storeInfo && (
+        <div className="flex flex-row items-center">
+          <img
+            src={getImgUrl(storeInfo.logoStore)}
+            alt="store-img"
+            className=" rounded-full object-cover w-[60px] h-[60px]"
+          />
+          <div className=" pl-4">
+            <div className=" text-base font-medium pb-1">{storeInfo.nameStore}</div>
+            <div className=" text-sm font-normal text-gray-500 pb-1">
+              {storeInfo.followers} theo dõi
             </div>
-            <div className=" pl-1">{storeInfo.address}</div>
+            <div className=" flex flex-row text-sm font-normal  text-gray-500">
+              <div className="flex items-center justify-center">
+                <Icon icon="zi-location-solid" size={12} />
+              </div>
+              <div className=" pl-1">{storeInfo.address}</div>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-    <Button className="chat-button" variant="primary" size="small" onClick={() => {}}>
-      Nhắn tin
-    </Button>
-  </div>
-);
+      )}
+      <Button className="chat-button" variant="primary" size="small" onClick={handleOpenChat}>
+        Nhắn tin
+      </Button>
+    </div>
+  );
+};
 
 export default CardShop;
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DEFAULT_OA_ID = '4318657068771012646';
